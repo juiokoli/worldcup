76fd6396e1dbc8e1832ae72efb40fe2d5478d57a
juiokoli <dominikstanek06@gmail.com>
2025-04-14 18:24:25 +0200

    Add files via upload

--- insert_data.sh
+++ insert_data.sh
@@ -13,4 +13,7 @@ tail -n +2 games.csv | while IFS=',' read -r year round winner opponent wg og
 do 
 $PSQL "insert into teams(name) values('$winner') on conflict do nothing"
 $PSQL "insert into teams(name) values('$opponent') on conflict do nothing"
+winner_id=$($PSQL "select team_id from teams where name='$winner'")
+opponent_id=$($PSQL "select team_id from teams where name='$opponent'")
+$PSQL "insert into games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) values($year,'$round',$winner_id,$opponent_id,$wg,$og)"
 done
